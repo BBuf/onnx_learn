fa1676068c02d46f57f31bb33760ed54140f20a6
BBuf <1182563586@qq.com>
2021-01-21 23:28:16 +0800

    init commit

---
+++ pytorch2onnx_resize.py
@@ -0,0 +1,15 @@
+import torch
+
+
+class JustReshape(torch.nn.Module):
+    def __init__(self):
+        super(JustReshape, self).__init__()
+
+    def forward(self, x):
+        return x.view((x.shape[0], x.shape[1], x.shape[3], x.shape[2]))
+
+
+net = JustReshape()
+model_name = 'just_reshape.onnx'
+dummy_input = torch.randn(2, 3, 4, 5)
+torch.onnx.export(net, dummy_input, model_name, input_names=['input'], output_names=['output'])
